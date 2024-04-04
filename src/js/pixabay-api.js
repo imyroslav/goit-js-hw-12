ca9b35d19284d00a3b10e943b50a87da5082a852
imyroslav <imyroslav@ukr.net>
2024-04-04 14:44:56 +0200

    axios-installed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,25 @@
+import axios from 'axios';
+
 const API_KEY = '43111947-de2162102976d485b612f31fc';
 const baseURL = 'https://pixabay.com/api/';
 
-export async function fetchImages(searchQuery) {
-  const url = `${baseURL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.hits;
+export async function fetchImages(searchQuery, page, perPage) {
+  try {
+    const response = await axios.get(baseURL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 }
